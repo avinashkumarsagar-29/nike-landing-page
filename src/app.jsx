d7ba2fdcc7faeb2fad71cc5f1f9224800056d8d6
avinashkumarsagar-29 <168345383+avinashkumarsagar-29@users.noreply.github.com>
2025-05-16 12:16:49 +0530

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import "./app.css";
+import Navigation from"./components/Navigation";
+import Sagar from "./components/Sagar";
+const App =() =>{
+  return <div>
+ <Navigation/>
+ <Sagar/>
+  </div>
+};
+export default App;
---
+++ src/components/Sagar.jsx
@@ -0,0 +1,35 @@
+const Sagar = () => {
+    return(
+        <main className="sagar">
+            <div className="sagar-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p> YOUR LOOK DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR CLOTH.WE PROVIDE THE BEST QUALITY CLOTH IN CHEAP RATES,ALSO PROVIDE THE 15% DISCOUNT IN ANY CLOTH. </p>
+
+                <div className="sagar-btn">
+                    <button> Shop NOW</button>
+                    <button className="sec-btn">Category</button>
+                     </div>
+                     <div className="shop">
+                        <p>Also Available On</p>
+
+                        <div className="brandlog">
+                            <img src="image/amazon.png" alt="amazon" />
+                            <img src="/image/flipkart.png" alt="flipkart" />
+                        </div>
+
+                     </div>
+            </div>
+            <div className="sagar-image"></div>
+         
+         <div>
+         <img src="/image/shoe_image.png" alt="sagar" />
+         </div>
+            
+
+            
+      
+            
+        </main>
+    );
+}
+export default Sagar;
